bbdb415ddf345dfb540bae4970d1b7e36f074c9e
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2024-06-17 18:28:49 +0530

    calculator with somepercent fault

--- faultyCalculator/script.js
+++ faultyCalculator/script.js
@@ -1,3 +1,47 @@
+let faultNumber = Math.random()
+console.log(faultNumber)
+
 let number1 = prompt("First Number");
 let number2 = prompt("second Number");
-
+let operator = prompt("What operation you want to perform? ")
+if(faultNumber > 0.9) {
+  let res
+  switch (operator) {
+    case "+":
+      res = +number1 + +number2 
+      console.log(`The result is ${res}`)
+      break;
+    case "-":
+      res = +number1 - +number2 
+      console.log(`The result is ${res}`)
+      break;
+    case "*":
+      res = +number1 * +number2 
+      console.log(`The result is ${res}`)
+        break;
+    default:
+      res = +number1 / +number2 
+      console.log(`The result is ${res}`)
+      break;
+  }
+} else {
+  let res
+  switch (operator) {
+    case "+":
+      res = +number1 - +number2 
+      console.log(`The result is ${res}`)
+      break;
+    case "-":
+      res = +number1 / +number2 
+      console.log(`The result is ${res}`)
+      break;
+    case "*":
+      res = +number1 + +number2 
+      console.log(`The result is ${res}`)
+        break;
+    default:
+      res = (+number1) ** +number2 
+      console.log(`The result is ${res}`)
+      break;
+  }
+}
