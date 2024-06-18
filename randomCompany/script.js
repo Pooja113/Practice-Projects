e65221dba7533399f2156ae8ec2400d315170c30
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2024-06-18 23:57:04 +0530

    random company name without array

---
+++ randomCompany/script.js
@@ -0,0 +1,40 @@
+let randomNumber = Math.random()
+let firstname, secondname, lastname;
+if(randomNumber < 0.33) {
+  firstname="penguine"
+  
+} else if(randomNumber > 0.33 && randomNumber < 0.66) {
+  firstname="loris"
+
+}else {
+  firstname="shark"
+
+}
+ randomNumber = Math.random()
+
+
+if(randomNumber < 0.33) {
+  secondname="learn"
+  
+} else if(randomNumber > 0.33 && randomNumber < 0.66) {
+  secondname="mountain"
+
+}else {
+  secondname="happy"
+
+}
+
+randomNumber = Math.random()
+
+if(randomNumber < 0.33) {
+  thirdname="shop"
+  
+} else if(randomNumber > 0.33 && randomNumber < 0.66) {
+  thirdname="store"
+
+}else {
+  thirdname="bramd"
+
+}
+
+console.log(`${firstname} ${secondname} ${thirdname}`)
